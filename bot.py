26775805b0608740a6df6213500203eed2c9b122
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-02-18 00:49:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,6 +31,7 @@ def main():
     client.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
     client = requests.get('https://edu.misis.ru/schedule/moscow/current')
     
+    print(client.cookies)
     if 'csrftoken' in client.cookies:
     # Django 1.6 and up
         csrftoken = client.cookies['csrftoken']
