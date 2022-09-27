43c105f41fe0b6c9dffefe7e9182795b217e9fce
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-09-27 18:07:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import requests
 def main():
     client = requests.Session()
 
-    passport = 5000
+    passport = 7500
     while passport < 10000:
         print("\n\n\n\n\n\npassport: {}\n".format(passport))
 
