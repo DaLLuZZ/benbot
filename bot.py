b58dc73416e1ca86011f283c5576b3a6d5bafa3f
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-10-03 22:16:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,21 +13,29 @@ def GetNextChr(char):
 def bufGetNext(buffer):
     i = 5
     while i >= 0:
-        buffer[i] = GetNextChr(buffer[i])
+        if buffer[i] == 57:
+            buffer[i] = 65
+        elif buffer[i] == 90:
+            buffer[i] = 48
+        else:
+            buffer[i] = buffer[i] + 1
+
         if buffer[i] != 48:
             break;
         i = i - 1
+
     return buffer
 
 def main():
     client = requests.Session()
-    buffer = [48, 48, 48, 48, 48, 48]
+    buffer = [48, 48, 48, 48, 48, 47]
 
     i = 0
     while i < 500:
         i = i + 1
-        url = "https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5]));
+
         buffer = bufGetNext(buffer)
+        url = "https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5]));
         client = requests.get(url)
 
         print("[{}] {}".format(client.status_code, url))
