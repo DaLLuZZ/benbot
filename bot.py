8984799c1d96cceeb18ba3ec3fe9e872c7e44472
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-10-03 23:36:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from datetime import datetime
+import calendar
 from prettytable import PrettyTable
 
 def bufGetNext(buffer):
@@ -22,6 +24,14 @@ def main():
     client = requests.Session()
 #    buffer = [48, 48, 48, 48, 48, 47]
 
+    table = PrettyTable()
+    table.field_names = ["№ п/п", "Наименование курса", "Ссылка для регистрации"]
+
+    log = open('log.txt', 'w')
+    outfile = open('output.txt', 'w')
+    outfiletable = open('table.txt', 'w')
+    outfiletable.write("Доступные для регистрации курсы на {}\n".format(datetime.utcfromtimestamp(int(calendar.timegm(datetime.utcnow().utctimetuple()))).strftime('%H:%M:%S %d-%m-%Y')))
+
 #    https://lms.misis.ru/enroll/HLTTBM
     buffer = [ord('H'), ord('L'), ord('T'), ord('T'), ord('B'), ord('A')]
 
@@ -33,11 +43,16 @@ def main():
         url = "https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5]));
         client = requests.get(url)
 
+        log.write("[{}] {}".format(client.status_code, url))
         print("[{}] {}".format(client.status_code, url))
         course = ""
         if client.status_code != 404: # course exists
             course = client.text[client.text.find("<title>Зарегистрироваться на ") + 29:client.text.find("</title>")]
-            print("FOUND: \n" + course)
+            outfile.write(url + "\n" + course)
+            table.add_row([number, course, url])
+            print("FOUND: " + course + "\n")
+
+    outfiletable.write(str(table))
 
 if __name__ == '__main__':
     main()
