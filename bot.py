ab540847929b3f639e98d6e7fe44540f1812b64a
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-10-03 21:55:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ def main():
     while i < 500:
         buffer = bufGetNext(buffer)
         print("https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5])))
-        i = i - 1
+        i = i + 1
     
 #    client = requests.get("https://lms.misis.ru/enroll/{}".format())
 
