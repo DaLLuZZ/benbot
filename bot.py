418afb2660b46eae2adc4962f1983a6c22fb1ad6
DaLLuZZ <79600653+DaLLuZZ@users.noreply.github.com>
2022-10-03 23:44:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,15 +44,15 @@ def main():
         url = "https://lms.misis.ru/enroll/{}{}{}{}{}{}".format(chr(buffer[0]), chr(buffer[1]), chr(buffer[2]), chr(buffer[3]), chr(buffer[4]), chr(buffer[5]));
         client = requests.get(url)
 
-        log.write("[{}] {}".format(client.status_code, url))
         print("[{}] {}".format(client.status_code, url))
         course = ""
         if client.status_code != 404: # course exists
             found = found + 1
             course = client.text[client.text.find("<title>Зарегистрироваться на ") + 29:client.text.find("</title>")]
             outfile.write(url + "\n" + course)
-            table.add_row([number, course, url])
+            table.add_row([found, course, url])
             print("FOUND: " + course + "\n")
+        log.write("[{}] [{}] [{}] {}".format(found, i, client.status_code, url))
 
     outfiletable.write(str(table))
 
